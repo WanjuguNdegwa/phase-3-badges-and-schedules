15f667a619fe4ebf775a3f4b4faf6522342072f1
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-06-12 13:32:39 +0300

    commit tree e8d8862cd166ca11125dcf096ea7260ce10daa2d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+badge_maker("Arel")
+
+def batch_badge_creator(names)
+  names.map do |name| 
+    badge_maker(name)
+  end
+end
+
+batch_badge_creator(["Arel", "Carol"])
+
+def assign_rooms(names)
+  room_assignments = names.map.with_index do |name, index|
+   "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+assign_rooms(["Arel", "Carol"])
+
+def printer(names)
+  names.each do |name| 
+    puts badge_maker(name)
+  end
+
+  assign_rooms(names).each do |name| 
+    puts name
+  end
+end
